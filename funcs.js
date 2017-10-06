2c70cb5e5bb25e2b400a4e72911fa773abbcf165
owdaa <awadodeh@gmail.com>
2017-10-06 16:10:15 -0500

    clean code

--- funcs.js
+++ funcs.js
@@ -84,15 +84,6 @@ module.exports.findNextMessage = function (inbox, lastHash) {
     }
   }
 
-  // for (var i = 0; i < inbox.messages.length; i += 1) {
-  //   console.log(inbox.messages[i].from);
-  //   console.log(inbox.messages[i].hash);
-  //   console.log(decode(inbox.messages[i].from));
-  //   console.log(fs.readFileSync(path.join(inbox.dir, inbox.messages[i].hash), "utf8"));
-  //   console.log(decode(fs.readFileSync(path.join(inbox.dir, inbox.messages[i].hash), 'utf8')));
-  //     console.log("--");
-  // }
-
   // read and decode the message
   return 'from: ' + decode(inbox.messages[found].from) + '\n---\n' +
     decode(fs.readFileSync(path.join(inbox.dir, inbox.messages[found].hash), 'utf8'))
